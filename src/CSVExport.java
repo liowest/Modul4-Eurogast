fc820c3fdcbfda71cd33eb4dbd9e7600835db2af
lionel.westreicher <lionel.westreicher@gmail.com>
2024-10-08 22:05:12 +0200

    das problem war wie bereits vermutet das format, ich habe ein neues format kreiert und weise es jeweils dem datum zu des artikels das kommt bevor er ausgegeben wird

--- src/CSVExport.java
+++ src/CSVExport.java
@@ -1,11 +1,15 @@
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 public class CSVExport implements Export {
     @Override
     public void export(ArrayList<Artikel> list) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
         System.out.println("Nummer;Bezeichnung;Einkaufspreis;Ablaufdatum;Kategorienummer");
         for(Artikel artikel : list){
-            System.out.println(artikel.getNummer()+";"+artikel.getBezeichnung()+";"+artikel.getEinkaufspreis()+";"+artikel.getAblaufdatum()+";"+artikel.getKategorienummer());
+            String date = sdf.format(artikel.getAblaufdatum().getTime());
+            System.out.println(artikel.getNummer()+";"+artikel.getBezeichnung()+";"+artikel.getEinkaufspreis()+";"+date+";"+artikel.getKategorienummer());
         }
     }
 }

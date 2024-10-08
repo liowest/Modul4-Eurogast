c1e43e8cdc3b74597ddaab719fff17ff8cbf2607
lionel.westreicher <lionel.westreicher@gmail.com>
2024-10-08 21:50:48 +0200

    Die JSONEXport Klasse sit nun fertig. Sie ist ähnlich wie die CSVEcport Klasse, sie implementiert das Interface Export und gibt mit einer for each schleife alle daten von den artikeln aus in form von Json. Außerdem habe ich noch einen kleinen zähler i eigebat der beim letzten Artikel kein  beistrich mehr setzten soll.

--- src/JSONExport.java
+++ src/JSONExport.java
@@ -5,16 +5,25 @@ public class JSONExport implements Export {
 
     @Override
     public void export(ArrayList<Artikel> list) {
-
+        System.out.println("[");
+        int i=0;
         for (Artikel artikel : list) {
             System.out.println("{");
-            System.out.println(" \"Nummer\": /"+artikel.getNummer()+",");
-            System.out.println("   \"Bezeichnung\": " + artikel.getBezeichnung() + ",");
-            System.out.println("   \"Einkaufspreis\": " + artikel.getEinkaufspreis() + ",");
-            System.out.println("   \"Ablaufdatum\": " + artikel.getAblaufdatum() + ",");
+            System.out.println(" \"Nummer\": "+artikel.getNummer()+",");
+            System.out.println("   \"Bezeichnung\": " + artikel.getBezeichnung()+",");
+            System.out.println("   \"Einkaufspreis\": " + artikel.getEinkaufspreis()+",");
+            System.out.println("   \"Ablaufdatum\": " + artikel.getAblaufdatum()+",");
             System.out.println("   \"Kategorienummer\": " + artikel.getKategorienummer());
-            System.out.print(" }");
-        }
 
+
+            if(i<list.size()){
+                System.out.println(" }");
+            }else {
+                System.out.println(" },");
+                i++;
+            }
+
+        }
+        System.out.println("]");
     }
 }
